19fa001a8db98f118ea2b1829697f55743633881
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2025-12-05 13:14:58 -0700

    Update test-4.sh

--- src/test-4.sh
+++ src/test-4.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-   echo "Retry count: $BUILDKITE_RETRY_COUNT"
+echo "Retry count: $BUILDKITE_RETRY_COUNT"
 
-      if [[ "$BUILDKITE_RETRY_COUNT" == "0" ]]; then
-        echo "Run 1 → failing with -1"
-        exit -1
-      elif [[ "$BUILDKITE_RETRY_COUNT" == "1" ]]; then
-        echo "Run 2 → failing with 143"
-        exit 143
-      elif [[ "$BUILDKITE_RETRY_COUNT" == "2" ]]; then
-        echo "Run 3 → failing with 143"
-        exit 143
-      elif [[ "$BUILDKITE_RETRY_COUNT" == "3" ]]; then
-        echo "Run 4 → failing with -1"
-        exit -1
-      else
-        echo "Run 5+ → success"
-        exit 0
-      fi
+if [[ "$BUILDKITE_RETRY_COUNT" == "0" ]]; then
+      echo "Run 1 → failing with -1"
+      exit -1
+elif [[ "$BUILDKITE_RETRY_COUNT" == "1" ]]; then
+       echo "Run 2 → failing with 143"
+       exit 143
+elif [[ "$BUILDKITE_RETRY_COUNT" == "2" ]]; then
+       echo "Run 3 → failing with 143"
+       exit 143
+elif [[ "$BUILDKITE_RETRY_COUNT" == "3" ]]; then
+       echo "Run 4 → failing with -1"
+       exit -1
+ else
+       echo "Run 5+ → success"
+       exit 0
+fi
