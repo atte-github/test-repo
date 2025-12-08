7e8d52f1b7da5b5b19acedfd4d809ca45723eb1a
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2025-12-08 11:09:33 -0700

    Update test-4.sh

--- src/test-4.sh
+++ src/test-4.sh
@@ -12,17 +12,20 @@ case $RETRY_COUNT in
   0)
     echo "❌ First attempt - exiting with -1"
     sleep 1
-    exit -1
+    # exit -1
+    exit 2
     ;;
   1)
     echo "❌ Second attempt - exiting with 143"
     sleep 1
-    exit 143
+    # exit 143
+    exit 2
     ;;
   2)
     echo "❌ Third attempt - exiting with -1"
     sleep 1
-    exit -1
+    # exit -1
+    exit 1
     ;;
   3)
     echo "❌ Fourth attempt - exiting with 143"
